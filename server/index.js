94939755ede894dc70a32d07aaca3d2a01b8e085
SANJOY <paulsanjoy2923@gmail.com>
2025-07-16 21:18:55 +0530

    added PORT and URL to .env

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.use("/api/v1/url", urlRouter)
+app.use("/url", urlRouter)
 
 const main = async () => {
     const PORT = process.env.PORT || 3000
